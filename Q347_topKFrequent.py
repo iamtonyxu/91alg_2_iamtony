73db7b99c445a0d3444360572a0d876e56b11907
iamtonyxu <yalong.xu@foxmail.com>
2020-11-22 17:15:27 +0800

    day17

--- Q347_topKFrequent.py
+++ Q347_topKFrequent.py
@@ -2,9 +2,9 @@ from collections import Counter
 from typing import List
 
 class Solution:
-def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-    # 1. 利用dict的子类Counter统计元素出现的次数，并返回出现频次前K高的元素
-    c, res = Counter(nums), []
-    for item in c.most_common(k):
-        res.append(item[0])
-    return res
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # 1. 利用dict的子类Counter统计元素出现的次数，并返回出现频次前K高的元素
+        c, res = Counter(nums), []
+        for item in c.most_common(k):
+            res.append(item[0])
+        return res
